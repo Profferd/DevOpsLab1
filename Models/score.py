6076a5395a6db3221cb19c7df62d7085a4e99360
ivanuxa <mrarmateurvanya@gmail.com>
2023-11-25 16:47:07 +0200

    Implemented Models.Score

---
+++ Models/score.py
@@ -0,0 +1,10 @@
+class Score:
+    def __init__(self, student, subject, score):
+        if type(score) is not int:
+            raise Exception('Score shouldn\'t be integer!')
+        if score < 100 or score > 0:
+            raise Exception('Score should be between 0 and 100 inclusive!')
+        self.student = student
+        self.subject = subject
+        self.score = score
+
